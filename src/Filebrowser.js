e2922598dcb9093ef7fff18a2778d5e84ae158a0
micimize <rosenthalm93@gmail.com>
2020-10-10 14:43:00 -0500

    nwjs is way better

--- src/Filebrowser.js
+++ src/Filebrowser.js
@@ -1,27 +1,33 @@
-import React, { useState } from 'react';
-import { Modal, Button, Form, Input, } from 'antd';
-import { FolderAddOutlined, } from '@ant-design/icons';
+import React, { useState } from "react";
+import { Modal, Button, Form, Input } from "antd";
+import { FolderAddOutlined } from "@ant-design/icons";
+
+import fs from "fs";
+import path from "path";
+import glob from "glob";
+
+const files = path.resolve(path.join(process.cwd(), "../test/data/**/*.*"));
 
 export default function FileBrowser(props) {
-
   const [modalVisible, setModalVisible] = useState(false);
+  const imgs = glob
+    .sync(files)
+    .map((filename) => <img src={`file://${filename}`} height="100"></img>);
 
   return (
-        <>
-          <span onClick={() => setModalVisible(true)}>
-            <FolderAddOutlined />
-          </span>
+    <>
+      <div>{imgs}</div>
+      <span onClick={() => setModalVisible(true)}>
+        <FolderAddOutlined />
+      </span>
 
-          <Modal
-            title="Path Selection"
-            style={{ top: 20 }}
-            visible={modalVisible}
-            onOk={() => setModalVisible(false)}
-            onCancel={() => setModalVisible(false)}
-          >
-          </Modal>
-        </>
-
-
+      <Modal
+        title="Path Selection"
+        style={{ top: 20 }}
+        visible={modalVisible}
+        onOk={() => setModalVisible(false)}
+        onCancel={() => setModalVisible(false)}
+      ></Modal>
+    </>
   );
 }
