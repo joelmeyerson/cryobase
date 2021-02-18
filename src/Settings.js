8fd308818b77f96520111d699abc2e55fef3fb4a
Joel <jrmeyerson@gmail.com>
2021-02-18 13:32:31 -0500

    Corrected package.json and Settings.js to reflect v1.0.1 update.

--- src/Settings.js
+++ src/Settings.js
@@ -42,7 +42,7 @@ export default function Settings(props) {
       <Row>
         <Col>
           <span>
-            <Tag>Version: 1.0.0</Tag>
+            <Tag>Version: 1.0.1</Tag>
           </span>
           <br></br>
           <span>
